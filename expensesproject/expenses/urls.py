575dfaf09700d4e450b70a74632adcd4f4f08936
Rohan271004 <rp285518@gmail.com>
2025-06-15 12:27:43 +0530

    Added preferences and user_preferences modules with supporting files

--- expensesproject/expenses/urls.py
+++ expensesproject/expenses/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name="expenses"),
-    path('add-expenses',views.add_expense, name="add-expenses")
+    path('add-expenses',views.add_expense, name="add-expenses"),
+    # path('edit-expense/<int:id>',views.expense_edit, name="expense-edit")
+    path('edit-expense/<int:id>', views.expense_edit, name="expense_edit"),
+    path('expense-delete/<int:id>', views.delete_expense, name="expense-delete")
+
 ]
--- expensesproject/expenses/views.py
+++ expensesproject/expenses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
 from django.contrib import messages
@@ -8,20 +8,89 @@ from django.contrib import messages
 @login_required(login_url='/authentication/login')
 def index(request):
     Categories = Category.objects.all()
-    return render(request, 'expenses/index.html')
+    expenses = Expense.objects.filter(owner=request.user)
+    
+    context={
+        'expenses':expenses
+    }
+    return render(request, 'expenses/index.html',context)
+
 
 @login_required(login_url='/authentication/login')  
 def add_expense(request):
     Categories = Category.objects.all()
+
     context = {
-        'categories' : Categories
-    }
-    
-    return render(request, 'expenses/add_expense.html', context)
+            'categories' : Categories,
+            'values': request.POST
+        }
+    if request.method == 'GET':
+
+        return render(request, 'expenses/add_expense.html', context)
 
     if request.method == 'POST':
         amount = request.POST['amount']
 
-    if not amount:
-        messages.error(request,"Amount is required")
-        return render(request, 'expenses/add_expenses.html', context)
+        if not amount:
+            messages.error(request,"Amount is required")
+            return render(request, 'expenses/add_expense.html', context)
+    
+        description = request.POST['description']
+        date = request.POST['expense-date']
+        category = request.POST['category']
+
+
+        if not description:
+            messages.error(request,"Description is required")
+            return render(request, 'expenses/add_expense.html', context)
+
+        Expense.objects.create(owner=request.user,amount=amount,date=date,category=category,description=description)
+        messages.success(request,'Expense saved successfully')
+
+        return redirect('expenses')
+    
+def expense_edit(request,id):
+    Categories = Category.objects.all()
+    expense = Expense.objects.get(pk=id)
+    context = {
+        'categories' : Categories,
+        'expense': expense,
+        'values' : expense
+    }
+    if request.method == 'GET':
+        
+        return render(request, 'expenses/edit-expense.html',context)
+    if request.method == 'POST':
+
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request,"Amount is required")
+            return render(request, 'expenses/edit-expense.html', context)
+    
+        description = request.POST['description']
+        date = request.POST['expense-date']
+        category = request.POST['category']
+
+
+        if not description:
+            messages.error(request,"Description is required")
+            return render(request, 'expenses/edit-expense.html', context)
+
+        
+        expense.owner = request.user
+        expense.amount = amount
+        expense.date = date
+        expense.description = description
+        expense.category = category
+
+        expense.save()
+        messages.success(request,'Expense updated successfully')
+
+        return redirect('expenses')
+
+def delete_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    expense.delete()
+    messages.success(request, "Expense removed")
+    return redirect('expenses')
